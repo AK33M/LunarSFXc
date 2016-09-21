61848b44b57c16a5cab73213c7f58e68d9ab48b8
Akeem Taiwo (Work) <akeem.taiwo@dataireland.ie>
2016-09-21 14:55:32 +0100

    add nuget dependencies set up MVC in StartUp.cs

--- src/LunarSFXc/Startup.cs
+++ src/LunarSFXc/Startup.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -12,25 +9,52 @@ namespace LunarSFXc
 {
     public class Startup
     {
+        private IHostingEnvironment _env;
+        private IConfigurationRoot _config;
+
+        public Startup(IHostingEnvironment env)
+        {
+            _env = env;
+
+            var builder = new ConfigurationBuilder()
+                            .SetBasePath(_env.ContentRootPath)
+                            .AddJsonFile("appSettings.json")
+                            .AddJsonFile("userSecrets.json")
+                            .AddEnvironmentVariables();
+
+            _config = builder.Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(_config);
+
+            //Add your Services here..
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
 
-            if (env.IsDevelopment())
+            if (_env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                loggerFactory.AddDebug(LogLevel.Information);
+            }
+            else
+            {
+                loggerFactory.AddDebug(LogLevel.Error);
             }
 
-            app.Run(async (context) =>
+            app.UseStaticFiles();
+
+            app.UseMvc(config =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}");
             });
         }
     }
