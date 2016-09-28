44cd38dd6fc85b2ba2e08f3f785689f7a5d83fd4
Akeem Taiwo (Work) <akeem.taiwo@dataireland.ie>
2016-09-28 15:53:11 +0100

    Login and Logout features

--- src/LunarSFXc/Controllers/AuthController.cs
+++ src/LunarSFXc/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
-﻿using LunarSFXc.ViewModels;
+﻿using LunarSFXc.Objects;
+using LunarSFXc.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -6,11 +8,73 @@ namespace LunarSFXc.Controllers
 {
     public class AuthController : Controller
     {
+        private SignInManager<LunarUser> _signInManager;
+        private UserManager<LunarUser> _userManager;
+
+        public AuthController(SignInManager<LunarUser> signInManager, UserManager<LunarUser> userManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel model, string returnUrl)
         {
             ViewBag.ReturnUrl = returnUrl;
-            return View();
+            if (ModelState.IsValid)
+            {
+                var signInResult = new Microsoft.AspNetCore.Identity.SignInResult();
+
+                if (model.Username.Contains("@"))
+                {
+                    var user = await _userManager.FindByEmailAsync(model.Username);
+
+                    if(user != null)
+                    {
+                        signInResult = await _signInManager.PasswordSignInAsync(
+                                                        user.UserName,
+                                                        model.Password,
+                                                        model.RememberMe,
+                                                        false);
+                    }                    
+                }
+                else
+                {
+                    signInResult = await _signInManager.PasswordSignInAsync(
+                                                        model.Username,
+                                                        model.Password,
+                                                        model.RememberMe,
+                                                        false);
+                }
+
+                
+                if (signInResult.Succeeded)
+                {
+                    if (string.IsNullOrWhiteSpace(returnUrl))
+                    {
+                        return RedirectToAction("Posts", "Blog");
+                    }
+                    else
+                    {
+                        return Redirect(returnUrl);
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Username/Email or Password incorrect");
+                }
+            }
+
+            return View("RedirectedLogin");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                await _signInManager.SignOutAsync();
+            }
+            return RedirectToAction("Posts", "Blog");
         }
     }
 }
