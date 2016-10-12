7e5c3a15f4fa494da648514c5d76df899ccf74e1
Akeem Taiwo (Work) <akeem.taiwo@dataireland.ie>
2016-10-12 13:08:59 +0100

    Added Logging to my Repo

--- src/LunarSFXc/Repositories/BlogRepository.cs
+++ src/LunarSFXc/Repositories/BlogRepository.cs
@@ -3,40 +3,101 @@ using LunarSFXc.Objects;
 using LunarSFXc.Contexts;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using System;
+using Microsoft.Extensions.Logging;
 
 namespace LunarSFXc.Repositories
 {
     public class BlogRepository : IBlogRepository
     {
         private readonly LunarDbContext _context;
+        private ILogger<BlogRepository> _logger;
 
-        public BlogRepository(LunarDbContext context)
+        public BlogRepository(LunarDbContext context, ILogger<BlogRepository> logger)
         {
+            _logger = logger;
             _context = context;
         }
         public ICollection<Post> Posts(int pageNo, int pageSize)
         {
-            //check if it includes Category and Tags
-            var posts = _context.Posts
-                                .Where(p => p.Published)
-                                .OrderByDescending(p => p.PostedOn)
-                                .Skip(pageNo * pageSize)
-                                .Take(pageSize)
-                                .Include(p => p.Category)
-                                .Include(p => p.PostTags)
-                                .Include(p => p.PostedBy)
-                                .ToList();
+            try
+            {
+                //check if it includes Category and Tags
+                var posts = _context.Posts
+                                    .Where(p => p.Published)
+                                    .OrderByDescending(p => p.PostedOn)
+                                    .Skip(pageNo * pageSize)
+                                    .Take(pageSize)
+                                    .Include(p => p.Category)
+                                    .Include(p => p.PostTags)
+                                    .Include(p => p.PostedBy)
+                                    .ToList();
 
-            GetTags(posts);
+                GetTags(posts);
 
-            return posts;
-
+                return posts;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error getting all Posts from database", ex);
+                return null;
+            }
         }
-
+        public ICollection<Category> Categories()
+        {
+            try
+            {
+                return _context.Categories.OrderBy(c => c.Name).ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error getting all Categories from database", ex);
+                return null;
+            }
+        }
+        public ICollection<Tag> Tags()
+        {
+            try
+            {
+                return _context.Tags.OrderBy(t => t.Name).ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error getting all Tags from database", ex);
+                return null;
+            }
+        }
         public ICollection<Post> PostsForTag(string tagSlug, int pageNo, int pageSize)
         {
-            var posts = _context.Posts
-                                .Where(p => p.Published && p.PostTags.Any(t => t.Tag.UrlSlug.Equals(tagSlug)))
+            try
+            {
+                var posts = _context.Posts
+                               .Where(p => p.Published && p.PostTags.Any(t => t.Tag.UrlSlug.Equals(tagSlug)))
+                               .OrderByDescending(p => p.PostedOn)
+                               .Skip(pageNo * pageSize)
+                               .Take(pageSize)
+                               .Include(p => p.Category)
+                               .Include(p => p.PostTags)
+                               .Include(p => p.PostedBy)
+                               .ToList();
+
+                GetTags(posts);
+
+                return posts;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error getting all Posts with Tag '{tagSlug}' from database", ex);
+                return null;
+            }
+
+        }
+        public ICollection<Post> PostsForCategory(string categorySlug, int pageNo, int pageSize)
+        {
+            try
+            {
+                var posts = _context.Posts
+                                .Where(p => p.Published && p.Category.UrlSlug.Equals(categorySlug))
                                 .OrderByDescending(p => p.PostedOn)
                                 .Skip(pageNo * pageSize)
                                 .Take(pageSize)
@@ -45,9 +106,115 @@ namespace LunarSFXc.Repositories
                                 .Include(p => p.PostedBy)
                                 .ToList();
 
-            GetTags(posts);
+                GetTags(posts);
+                return posts;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error getting all Posts for Category '{categorySlug}' from database", ex);
+                return null;
+            }
+        }
+        public ICollection<Post> PostsForSearch(string search, int pageNo, int pageSize)
+        {
+            try
+            {
+                var posts = _context.Posts
+                               .Where(p => p.Published && (p.Title.Contains(search)
+                                                           || p.Category.Name.Equals(search)
+                                                           || p.PostTags.Any(t => t.Tag.Name.Equals(search))))
+                               .OrderByDescending(p => p.PostedOn)
+                               .Skip(pageNo * pageSize)
+                               .Take(pageSize)
+                               .Include(p => p.Category)
+                               .Include(p => p.PostTags)
+                               .Include(p => p.PostedBy)
+                               .ToList();
 
-            return posts;
+                GetTags(posts);
+
+                return posts;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error getting all Posts with search term '{search}' from database", ex);
+                return null;
+            }
+        }
+
+        public Tag Tag(string tagSlug)
+        {
+            try
+            {
+                var tag = _context.Tags
+                                .FirstOrDefault(t => t.UrlSlug.Equals(tagSlug));
+
+                return tag;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error getting Tag '{tagSlug}' from database", ex);
+                return null;
+            }
+        }
+        public Post Post(int year, int month, string titleSlug)
+        {
+            try
+            {
+                var query = _context.Posts
+                                .Where(p => p.PostedOn.Year == year && p.PostedOn.Month == month && p.UrlSlug.Equals(titleSlug))
+                                .Include(p => p.Category)
+                                .Include(p => p.PostTags)
+                                .Include(p => p.PostedBy)
+                                .ToList();
+
+                GetTags(query);
+
+                return query.FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error getting Posts with title '{titleSlug}' from database", ex);
+                return null;
+            }
+        }
+        public Category Category(string categorySlug)
+        {
+            try
+            {
+                return _context.Categories.FirstOrDefault(t => t.UrlSlug.Equals(categorySlug));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error getting Category '{categorySlug}' from database", ex);
+                return null;
+            }
+        }
+
+        public int TotalPostsForSearch(string search)
+        {
+            return _context.Posts
+                           .Where(p => p.Published && (p.Title.Contains(search)
+                                                        || p.Category.Name.Equals(search)
+                                                        || p.PostTags.Any(t => t.Tag.Name.Equals(search))))
+                           .Count();
+        }
+        public int TotalPosts()
+        {
+            return _context.Posts
+                            .Where(p => p.Published)
+                            .Count();
+        }
+        public int TotalPostsForTag(string tagSlug)
+        {
+            return _context.Posts
+                            .Where(p => p.Published && p.PostTags.Any(t => t.Tag.UrlSlug.Equals(tagSlug)))
+                            .Count();
+        }
+        public int TotalPostsForCategory(string categorySlug)
+        {
+            return _context.Posts.Where(p => p.Published && p.Category.UrlSlug.Equals(categorySlug))
+                                .Count();
         }
 
         private void GetTags(List<Post> posts)
@@ -63,108 +230,7 @@ namespace LunarSFXc.Repositories
                     }
                 }
             }
-        }
 
-        public Tag Tag(string tagSlug)
-        {
-            var tag = _context.Tags
-                                .FirstOrDefault(t => t.UrlSlug.Equals(tagSlug));
-
-            return tag;
-        }
-
-        public int TotalPosts()
-        {
-            return _context.Posts
-                            .Where(p => p.Published)
-                            .Count();
-        }
-
-        public int TotalPostsForTag(string tagSlug)
-        {
-            return _context.Posts
-                            .Where(p => p.Published && p.PostTags.Any(t => t.Tag.UrlSlug.Equals(tagSlug)))
-                            .Count();
-        }
-
-        public ICollection<Post> PostsForCategory(string categorySlug, int pageNo, int pageSize)
-        {
-            var posts = _context.Posts
-                                .Where(p => p.Published && p.Category.UrlSlug.Equals(categorySlug))
-                                .OrderByDescending(p => p.PostedOn)
-                                .Skip(pageNo * pageSize)
-                                .Take(pageSize)
-                                .Include(p => p.Category)
-                                .Include(p => p.PostTags)
-                                .Include(p => p.PostedBy)
-                                .ToList();
-
-            GetTags(posts);
-            return posts;
-        }
-
-        public int TotalPostsForCategory(string categorySlug)
-        {
-            return _context.Posts.Where(p => p.Published && p.Category.UrlSlug.Equals(categorySlug))
-                                .Count();
-        }
-
-        public Category Category(string categorySlug)
-        {
-            return _context.Categories.FirstOrDefault(t => t.UrlSlug.Equals(categorySlug));
-        }
-
-        public ICollection<Post> PostsForSearch(string search, int pageNo, int pageSize)
-        {
-            var posts = _context.Posts
-                                .Where(p => p.Published && (p.Title.Contains(search)
-                                                            || p.Category.Name.Equals(search)
-                                                            || p.PostTags.Any(t => t.Tag.Name.Equals(search))))
-                                .OrderByDescending(p => p.PostedOn)
-                                .Skip(pageNo * pageSize)
-                                .Take(pageSize)
-                                .Include(p => p.Category)
-                                .Include(p => p.PostTags)
-                                .Include(p => p.PostedBy)
-                                .ToList();
-
-            GetTags(posts);
-
-            return posts;
-
-        }
-
-        public int TotalPostsForSearch(string search)
-        {
-            return _context.Posts
-                           .Where(p => p.Published && (p.Title.Contains(search)
-                                                        || p.Category.Name.Equals(search)
-                                                        || p.PostTags.Any(t => t.Tag.Name.Equals(search))))
-                           .Count();
-        }
-
-        public Post Post(int year, int month, string titleSlug)
-        {
-            var query = _context.Posts
-                                .Where(p => p.PostedOn.Year == year && p.PostedOn.Month == month && p.UrlSlug.Equals(titleSlug))
-                                .Include(p => p.Category)
-                                .Include(p => p.PostTags)
-                                .Include(p => p.PostedBy)
-                                .ToList();
-
-            GetTags(query);
-
-            return query.FirstOrDefault();
-        }
-
-        public ICollection<Category> Categories()
-        {
-            return _context.Categories.OrderBy(c => c.Name).ToList();
-        }
-
-        public ICollection<Tag> Tags()
-        {
-            return _context.Tags.OrderBy(t => t.Name).ToList();
         }
     }
 }
