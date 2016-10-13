4eba6d495824320eed159bd1b07a00714de7a1fd
Akeem Taiwo (Work) <akeem.taiwo@dataireland.ie>
2016-10-13 11:38:06 +0100

    Comment Form ViewComponent

---
+++ src/LunarSFXc/ViewComponents/CommentForm.cs
@@ -0,0 +1,15 @@
+﻿using LunarSFXc.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LunarSFXc.ViewComponents
+{
+    public class CommentForm : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var model = new CommentViewModel();
+
+            return View("CommentForm", model);
+        }
+    }
+}
---
+++ src/LunarSFXc/ViewModels/CommentViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LunarSFXc.ViewModels
+{
+    public class CommentViewModel
+    {
+        [Required]
+        [DataType(DataType.MultilineText)]
+        [StringLength(maximumLength: 250)]
+        public string Comment { get; set; }
+    }
+}
