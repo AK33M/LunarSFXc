e251277abafbd0e74b67fcaf334a29edbe448710
Akeem Taiwo (Work) <akeem.taiwo@dataireland.ie>
2016-10-14 18:08:37 +0100

    [Authorize]

--- src/LunarSFXc/Controllers/AdminController.cs
+++ src/LunarSFXc/Controllers/AdminController.cs
@@ -1,16 +1,18 @@
 ﻿using LunarSFXc.Repositories;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LunarSFXc.Controllers
 {
     //[RequireHttps]
+    [Authorize]
     public class AdminController : Controller
     {
         private IBlogRepository _repo;
 
         public AdminController(IBlogRepository repo)
         {
-            _repo= repo;
+            _repo = repo;
         }
 
         public IActionResult Index()
