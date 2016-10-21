59137e440c8c0e2f8505cc3d5dfcdb9323ca3c3c
Akeem Taiwo (Work) <akeem.taiwo@dataireland.ie>
2016-10-21 17:28:55 +0100

    Frontend Image Gallery

--- src/LunarSFXc/ViewComponents/PhotoGallery.cs
+++ src/LunarSFXc/ViewComponents/PhotoGallery.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LunarSFXc.Services;
+using LunarSFXc.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace LunarSFXc.ViewComponents
 {
     public class PhotoGallery : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private ICouldStorageService _cloudService;
+
+        public PhotoGallery(ICouldStorageService cloudService)
         {
-            return View("PhotoGallery");
+            _cloudService = cloudService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var uris = await _cloudService.ListAllBlobs("imagesupload");
+
+            var model = new GalleryViewModel
+            {                
+                 ImageUris = uris
+            };
+
+            return View("PhotoGallery", model);
         }
     }
 }
---
+++ src/LunarSFXc/ViewModels/GalleryViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LunarSFXc.ViewModels
+{
+    public class GalleryViewModel
+    {
+        public ICollection<Uri> ImageUris { get; set; }
+    }
+}
