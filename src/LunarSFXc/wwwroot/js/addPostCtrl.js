b6c7b41410e8de2fc63b74cc17edd2c6d4fcc46c
Akeem Taiwo (Work) <akeem.taiwo@dataireland.ie>
2016-11-03 11:52:37 +0000

    add blog post form

--- src/LunarSFXc/wwwroot/js/addPostCtrl.js
+++ src/LunarSFXc/wwwroot/js/addPostCtrl.js
@@ -1,13 +1,56 @@
-﻿(function(){
+﻿(function () {
     "use strict";
 
 
     angular
         .module("app-admin")
-        .controller("AddPostCtrl", ["$scope", "dataResource", AddPostCtrl]);
+        .controller("AddPostCtrl", ["$scope", "$location", "$routeParams", "dataResource", "FileUploader", AddPostCtrl]);
 
-    function AddPostCtrl($scope, dataResource) {
+    function AddPostCtrl($scope, $location, $routeParams, dataResource, FileUploader) {
         var vm = this;
         $scope.greeting = "Hello Form";
+
+
+
+
+
+
+
+
+        //AngularFileUpload http://nervgh.github.io/pages/angular-file-upload/examples/simple/
+        // Creates a uploader
+        var uploader = $scope.uploader = new FileUploader({
+            scope: $scope,
+            url: 'api/images/upload',
+            queueLimit: 1,
+            removeAfterUpload: true,
+            autoUpload: true,
+            formData: [{
+                containerName: 'aboutmetimeline'
+            }]
+        });
+
+        // Registers a filter: images only
+        uploader.filters.push({
+            name: 'imageFilter',
+            fn: function (item, options) {
+                var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+                return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+            }
+        });
+
+        //CALL BACKS
+        uploader.onProgressItem = function (fileItem, progress) {
+            //console.info('onProgressItem', fileItem, progress);
+        };
+
+        uploader.onSuccessItem = function (fileItem, response, status, headers) {
+            console.info('onSuccessItem', fileItem, response, status, headers);
+            $scope.aboutMeEvent.image = response.image;
+        };
+
+        uploader.onErrorItem = function (fileItem, response, status, headers) {
+            //console.info('onErrorItem', fileItem, response, status, headers);
+        };
     };
 }());
